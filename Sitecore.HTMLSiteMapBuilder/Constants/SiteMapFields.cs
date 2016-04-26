efb469655f8f792259056f467f073f9f4b6583e1
RyanABailey <ryan.bailey@windowslive.com>
2016-04-26 15:59:34 +1200

    Update SiteMapFields.cs
    Show In SiteMap -> Show In XML SiteMap

--- Sitecore.HTMLSiteMapBuilder/Constants/SiteMapFields.cs
+++ Sitecore.HTMLSiteMapBuilder/Constants/SiteMapFields.cs
@@ -8,7 +8,7 @@ namespace Sitecore.AdvancedSiteMap.Constants
 {
     public class SiteMapFields
     {
-        public const string ShowInSiteMap = "Show In SiteMap";
+        public const string ShowInSiteMap = "Show In XML SiteMap";
         public const string FrequencyChange = "Frequency Change";
         public const string Priority = "Priority";
         public const string ShowInHTMLSiteMap = "Show In HTML SiteMap";
